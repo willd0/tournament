b44db995f31e353e939e4bc5d01f783247c6ec3b
Will <willd2122@gmail.com>
2021-11-08 17:08:23 -0600

    added name to rounds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_06_034936) do
+ActiveRecord::Schema.define(version: 2021_11_08_230745) do
 
   create_table "brackets", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2021_11_06_034936) do
   create_table "rounds", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
 end
